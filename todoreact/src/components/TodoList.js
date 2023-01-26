c27fc13a91852ed6bbf89088f17f7ebccac71ac7
Simernor <simonnorenius@gmail.com>
2023-01-26 10:36:02 +0100

    reactstart

---
+++ todoreact/src/components/TodoList.js
@@ -0,0 +1,18 @@
+function TodoList()  {
+	
+    return(
+        <div className="todoListMain">
+          <div className="header">
+            <form >
+              <input  placeholder="Vad behöver du göra?">
+              </input>
+              <button type="submit">+</button>
+            </form>
+            
+          </div>
+            </div>
+    );
+}
+
+
+export default TodoList;
